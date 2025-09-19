a1b73b0cad8768b4fd246904edd24d9158e97a65
ahmedyousry0098 <ahmedyousry098@gmail.com>
2025-09-19 14:28:11 +0300

    solve reverse integer

---
+++ src/ReverseInteger/Main.java
@@ -0,0 +1,11 @@
+package ReverseInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int reversed = solution.reverse(96463243);
+
+        System.out.println(reversed);
+    }
+}
